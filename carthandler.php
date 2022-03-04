d644d17c61840368fbd09cddef99b54294e43710
sharifsharif101 <55722494+sharifsharif101@users.noreply.github.com>
2022-03-04 12:06:10 +0200

    3-3 new work

--- carthandler.php
+++ carthandler.php
@@ -1,21 +1,37 @@
 <?php 
 session_start();
 if (isset($_SESSION['cart'])){
+    $checker=array_column($_SESSION['cart'],'item_name');
+    if (in_array($_GET['cart_name'],$checker)) {
+        echo "<script>alert('product Is already in the cart');
+        window.location.href='product.php';
+        </script>";
+    }
+
+
+
+
+
     $count=count($_SESSION['cart']);
     $_SESSION['cart'][$count]=array('
     item_id'=>$_GET['cart_id'],
     'item_name'=>$_GET['cart_name'],
     'item_price'=>$_GET['cart_price'] 
 );
+echo "<script>alert('product added');
+window.location.href='product.php';
+</script>";
+
 }else{
     $_SESSION['cart'][0]=array(
         'item_id'=>$_GET['cart_id'],
         'item_name'=>$_GET['cart_name'],
         'item_price'=>$_GET['cart_price'] );
+    
+     echo "<script>alert('product added');
+     window.location.href='product.php';
+     </script>";
 }
-
-print_r($_SESSION['cart']);
-
 
 
 ?>
