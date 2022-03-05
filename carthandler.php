9df7dd4390edaeda769d483fc87c22500d06a61d
sharifsharif101 <55722494+sharifsharif101@users.noreply.github.com>
2022-03-06 01:35:24 +0200

    3-6-2022

--- carthandler.php
+++ carthandler.php
@@ -1,4 +1,5 @@
 <?php 
+
 session_start();
 if (isset($_SESSION['cart'])){
     $checker=array_column($_SESSION['cart'],'item_name');
@@ -6,32 +7,31 @@ if (isset($_SESSION['cart'])){
         echo "<script>alert('product Is already in the cart');
         window.location.href='product.php';
         </script>";
-    }
-
-
-
-
-
+     
+    }else{
     $count=count($_SESSION['cart']);
     $_SESSION['cart'][$count]=array('
     item_id'=>$_GET['cart_id'],
     'item_name'=>$_GET['cart_name'],
-    'item_price'=>$_GET['cart_price'] 
+    'item_price'=>$_GET['cart_price'],
+    'quantity'=>1
 );
+    
 echo "<script>alert('product added');
 window.location.href='product.php';
 </script>";
-
+   }
 }else{
     $_SESSION['cart'][0]=array(
         'item_id'=>$_GET['cart_id'],
         'item_name'=>$_GET['cart_name'],
-        'item_price'=>$_GET['cart_price'] );
+        'item_price'=>$_GET['cart_price'],
+        'quantity'=>1 
+    );
     
      echo "<script>alert('product added');
      window.location.href='product.php';
      </script>";
 }
-
 
 ?>
